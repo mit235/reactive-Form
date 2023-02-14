1beccba5bf5ebdd0444114503a58ba0062b76baa
mit <meetlimbani5@gmail.com>
2023-02-14 18:28:38 +0530

    reactive Form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{FormControl,FormGroup,Validators}from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+ loginForm=new FormGroup(
+  {
+    user:new FormControl("",[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
+  }
+ )
+ loginUser(item:any){
+  console.log(item)
+ }
+ get users(){
+  return this.loginForm.get('user');
+ }
+ get pass(){
+  return this.loginForm.get('password');
+ }
 }
